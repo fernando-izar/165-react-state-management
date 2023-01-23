0809648cefbcc4a83fadc27d90a811b1f9022179
fernando-izar <fizar@me.com>
2023-01-23 06:00:43 -0300

    feat: alterar quantidades no carrinho funcionando

--- src/store/reducers/carrinho.js
+++ src/store/reducers/carrinho.js
@@ -18,9 +18,17 @@ const carrinhoSlice = createSlice({
         ];
       return state.filter((item) => item.id !== payload);
     },
+    mudarQuantidade: (state, { payload }) => {
+      state = state.map((itemNoCarrinho) => {
+        if (itemNoCarrinho.id === payload.id) {
+          itemNoCarrinho.quantidade += payload.quantidade;
+        }
+        return itemNoCarrinho;
+      });
+    },
   },
 });
 
-export const { mudarCarrinho } = carrinhoSlice.actions;
+export const { mudarCarrinho, mudarQuantidade } = carrinhoSlice.actions;
 
 export default carrinhoSlice.reducer;
